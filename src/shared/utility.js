bec0bafd0022b68c909c3fad913d12eb060bfc32
VanDang220695 <nguyenvandang22061995@gmail.com>
2020-03-30 22:04:49 +0700

    Refactor code

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,26 @@ export const updateObject = (oldObject, updateProperties) => {
     ...updateProperties,
   };
 };
+export const checkValidity = (value, rules) => {
+  let isValid = true;
+  if (!rules) return isValid;
+  if (rules.required) {
+    isValid = value.trim() !== '' && isValid;
+  }
+  if (rules.minLength) {
+    isValid = value.trim().length >= rules.minLength && isValid;
+  }
+  if (rules.maxLength) {
+    isValid = value.trim().length <= rules.maxLength && isValid;
+  }
+  if (rules.isEmail) {
+    // eslint-disable-next-line no-useless-escape
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    isValid = pattern.test(value) && isValid;
+  }
+  if (rules.isNumeric) {
+    const pattern = /^\d+$/;
+    isValid = pattern.test(value) && isValid;
+  }
+  return isValid;
+};
